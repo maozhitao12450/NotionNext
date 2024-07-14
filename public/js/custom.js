5f1b30da5161f1640c2145ec84fe9e492cd8b491
Barry Sheneman <35758682+maozhitao12450@users.noreply.github.com>
2024-07-14 20:08:29 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,8 +3,9 @@ var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 s1.async=true;
-s1.src='https://embed.tawk.to/66924c3832dca6db2caecb0d/1i2lnjg61';
+s1.src='https://embed.tawk.to/6693bf6632dca6db2caf30ce/1i2oi7ara';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
 })();
+</script>
