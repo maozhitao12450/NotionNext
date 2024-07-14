55cd3462dc629dcfcd4e108dbdf40a48ee3a0ba6
Barry Sheneman <35758682+maozhitao12450@users.noreply.github.com>
2024-07-14 18:02:43 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,10 @@
-// 这里编写自定义js脚本；将被静态引入到页面中
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/66924c3832dca6db2caecb0d/1i2lnjg61';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
